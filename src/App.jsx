3b34e2f0de5f86225c1c65fef7cc1ecf5cf21cfd
rasyidahnuraini <rasyidahnuraini3156@gmail.com>
2026-09-24 14:18:45 +0700

    ubah teks di app

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1 className="bg-red-300">Vite + React</h1>
+      <h3>Rasyidah Nuraini</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
